8707484fcdbbc599ed1c13e5e06098fd55506808
minus3theta <yone_j_synthesis@yahoo.co.jp>
2022-12-13 03:31:04 +0900

    Add 11

---
+++ src/bin/11b.rs
@@ -0,0 +1,155 @@
+use std::str::FromStr;
+
+use anyhow::{bail, Context};
+use ezio::stdio;
+use itertools::Itertools;
+
+fn gcd(x: i64, y: i64) -> i64 {
+    if y == 0 {
+        x
+    } else {
+        gcd(y, x % y)
+    }
+}
+
+fn lcm(x: i64, y: i64) -> i64 {
+    x * y / gcd(x, y)
+}
+
+fn main() -> anyhow::Result<()> {
+    let input = stdio::stdin().into_iter().join("\n");
+    let input = input
+        .split("\n\n")
+        .map(|s| s.parse())
+        .collect::<Result<Vec<State>, _>>()?;
+    let (mut state, monkey): (Vec<Items>, Vec<Monkey>) =
+        input.into_iter().map(|State(s, m)| (s, m)).unzip();
+    let mut div = 1;
+    for m in &monkey {
+        div = lcm(div, m.divisor);
+    }
+    let mut count = vec![0; monkey.len()];
+    for _ in 0..10000 {
+        for (i, m) in monkey.iter().enumerate() {
+            let mut items = vec![];
+            std::mem::swap(&mut items, &mut state[i]);
+            count[i] += items.len();
+            m.process(items, &mut state, div);
+        }
+    }
+    count.sort();
+    let ans = count.pop().unwrap() * count.pop().unwrap();
+    println!("{}", ans);
+
+    Ok(())
+}
+
+#[derive(Debug)]
+enum Operation {
+    Add(i64),
+    Mult(i64),
+    Square,
+}
+
+impl Operation {
+    fn apply(&self, old: i64) -> i64 {
+        match self {
+            Operation::Add(r) => old + r,
+            Operation::Mult(r) => old * r,
+            Operation::Square => old * old,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Monkey {
+    operation: Operation,
+    divisor: i64,
+    throw_true: usize,
+    throw_false: usize,
+}
+
+impl Monkey {
+    fn process(&self, items: Items, state: &mut [Items], div: i64) {
+        for item in items {
+            let value = self.operation.apply(item) % div;
+            let destination = if value % self.divisor == 0 {
+                self.throw_true
+            } else {
+                self.throw_false
+            };
+            state[destination].push(value);
+        }
+    }
+}
+
+type Items = Vec<i64>;
+
+#[derive(Debug)]
+struct State(Items, Monkey);
+
+impl FromStr for State {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.lines();
+        iter.next();
+        let (_, items) = iter
+            .next()
+            .context("no items")?
+            .split(": ")
+            .collect_tuple()
+            .context("invalid items")?;
+        let items = items
+            .split(", ")
+            .map(FromStr::from_str)
+            .collect::<Result<Vec<_>, _>>()?;
+        let (_, operation) = iter
+            .next()
+            .context("no operation")?
+            .split("= old ")
+            .collect_tuple()
+            .context("invalid operation")?;
+        let operation = match operation
+            .split_ascii_whitespace()
+            .collect_tuple()
+            .context("invalid operation")?
+        {
+            ("*", "old") => Operation::Square,
+            ("*", rhs) => Operation::Mult(rhs.parse()?),
+            ("+", rhs) => Operation::Add(rhs.parse()?),
+            _ => bail!("invalid operation"),
+        };
+        let divisor = iter
+            .next()
+            .context("no test")?
+            .split_ascii_whitespace()
+            .last()
+            .context("no test")?
+            .parse()?;
+        let throw_true = iter
+            .next()
+            .context("no throw_true")?
+            .split_ascii_whitespace()
+            .last()
+            .context("no throw_true")?
+            .parse()?;
+        let throw_false = iter
+            .next()
+            .context("no throw_false")?
+            .split_ascii_whitespace()
+            .last()
+            .context("no throw_false")?
+            .parse()?;
+
+        Ok(Self(
+            items,
+            Monkey {
+                operation,
+                divisor,
+                throw_true,
+                throw_false,
+            },
+        ))
+    }
+}
